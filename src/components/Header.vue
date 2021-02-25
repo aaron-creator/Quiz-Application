977e4e281408bba630a249a156c67d915600ad10
aaron_tetracube <aaronpsam@gmail.com>
2021-02-25 15:22:58 +0530

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item disabled><strong> Fancy Quiz Application</strong> </b-nav-item>
+            <b-nav-item disabled>Counter : 4/10</b-nav-item>
+        </b-nav>
+    </div>
+</template>
